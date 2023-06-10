c991e51a474184605bf2004ee103a10520a66a99
LinHong <linlin.hong1@gmail.com>
2023-06-10 09:45:45 -0400

    homescreen html finished

--- gooselife/screens/HomeScreen.jsx
+++ gooselife/screens/HomeScreen.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
 import { StyleSheet, View, SafeAreaView, Image, Text } from 'react-native'
 
-const FoodWidget = () => {
-  return (
-    <Text>Food</Text>
-  )
-}
+const TimedTask = ({ isMeal }) => {
+    return (
+      <View style={styles.timedTask}>
+          <View style={styles.timedTask}>
+              <Text style={styles.timedTask}>{isMeal ? 'Meal' : 'Meal'} Time</Text>
+              <View style={styles.timedTask}>
+                  <Text style={styles.timedTask}>{isMeal ? 'Lunch' : 'Sleep'} - </Text>
+                  <Text style={styles.timedTask}>in 30 minutes</Text>
+              </View>
+              <View style={styles.timedTask}>
+                  <View style={styles.timedTask}></View>
+                  <View style={styles.timedTask}></View>
+              </View>
+          </View>
+  
+          <View style={styles.timedTask}>
+              <Text style={styles.timedTask}>I ate</Text>
+          </View>
+      </View>
+    )
+  }
+
+  const HygieneTask = () => {
+    return (
+      <View style={styles.hygieneTask}>
+          <View style={styles.hygieneTask}>
+              <Text style={styles.hygieneTask}>Hygiene</Text>
+              <Text style={styles.hygieneTask}>Take a shower</Text>
+          </View>
+  
+          <View style={styles.hygieneTask}>
+              <Text style={styles.hygieneTask}>I did</Text>
+          </View>
+      </View>
+    )
+  }
+
+  const ProgressTask = ({ isStudy }) => {
+    return (
+      <View style={styles.progressTask}>
+          <View style={styles.progressTask}>
+              <Text style={styles.progressTask}>{isStudy? 'Study Minutes' : 'Steps'}</Text>
+              <View>
+                <Text style={styles.progressTask}>##/</Text>
+                <Text style={styles.progressTask}>##</Text>
+              </View>
+          </View>
+  
+          <View style={styles.progressTask}>
+              <Text style={styles.progressTask}>Start</Text>
+          </View>
+      </View>
+    )
+  }
 
 export default function HomeScreen() {
     return (
@@ -26,9 +75,11 @@ export default function HomeScreen() {
                 <Text style={styles.fact}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi earum accusantium nesciunt nisi eaque blanditiis omnis saepe autem, est, amet provident error eligendi. Fuga iure ab accusamus est necessitatibus voluptatibus.</Text>
             </View>
 
-            <View>
-                <Text>Earn points</Text>
-                <FoodWidget/>
+            <View style={styles.task}>
+                <Text style={styles.task}>Earn points</Text>
+                <TimedTask isMeal={true}/>
+                <HygieneTask/>
+                <ProgressTask isStudy={false}/>
             </View>
         </SafeAreaView>
     );
